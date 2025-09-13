9ef43c6ba16a2cf5b1bf5d2318f9c90693890d9f
roboraton <133822199+roboraton@users.noreply.github.com>
2025-09-12 22:11:13 -0600

    BOM - fav passages activity commit

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector ('#favchap');
+const button = document.querySelector ('button');
+const list = document.querySelector ('#list');
+
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement ('li');
+        const deleteButton = document.createElement ('button');
+        
+        li.textContent = input.value; 
+        deleteButton.textContent = '❌';
+        
+        li.append(deleteButton);
+        
+        list.append(li);
+
+        button.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+
+        input.focus();
+
+    }
+});
