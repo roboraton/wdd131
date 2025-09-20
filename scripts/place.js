930ad4681475c165bff425bc59449ba563dcf2ef
roboraton <133822199+roboraton@users.noreply.github.com>
2025-09-20 14:33:51 -0600

    Week 3 exersices

---
+++ scripts/place.js
@@ -0,0 +1,24 @@
+const year = document.getElementById("currentyear");
+const lastModified = document.getElementById("lastmodified");
+
+const today = new Date();
+year.textContent = `${today.getFullYear()}`;
+lastModified.textContent = document.lastModified;
+
+function calculateWindChill(tempC, speed) {
+    if (tempC <= 10 && speed > 4.8) {
+        return Math.round(
+            13.12 + (0.6215 * tempC) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempC * Math.pow(speed, 0.16))
+        );
+    } else {
+        return "N/A";
+    }
+}
+
+// Static values for demonstration
+const temp = 25; // Temperature in °C
+const windSpeed = 15; // Wind speed in km/h
+
+// HTML call
+const windChill = calculateWindChill(temp, windSpeed);
+document.getElementById("windchill").textContent = windChill;
