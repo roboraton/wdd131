b4c396efe4ad0eabb6526736af8a568c851829be
roboraton <133822199+roboraton@users.noreply.github.com>
2025-09-25 17:24:16 -0600

    Lazy loading basic layout

---
+++ week04/scripts/scripts.js
@@ -0,0 +1,6 @@
+const year = document.getElementById("currentyear");
+const lastModified = document.getElementById("lastmodified");
+
+const today = new Date();
+year.textContent = `${today.getFullYear()}`;
+lastModified.textContent = document.lastModified;
